9a7bdb7fedd25b5ea4f620b65a637110c0894065
r2obd <45241943+r2obd@users.noreply.github.com>
2022-11-01 18:25:55 +0000

    tea time

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
-          elevation: 0.2,
+          elevation: 1,
           iconTheme: IconThemeData(
             color: Colors.grey, //change your color here
           ),
@@ -30,9 +30,9 @@ class MyApp extends StatelessWidget {
           primary: Colors.white,
           onPrimary: Color.fromARGB(210, 95, 100, 222),
           // Colors that are not relevant to AppBar in LIGHT mode:
-          primaryVariant: Colors.white,
-          secondary: Colors.white,
-          secondaryVariant: Colors.white,
+          primaryContainer: Colors.white,
+          secondary: Colors.grey,
+          secondaryContainer: Colors.grey,
           onSecondary: Colors.grey,
           background: Colors.white,
           onBackground: Colors.grey,
@@ -55,16 +55,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -76,12 +66,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 75,
@@ -123,15 +107,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ]
         ),
-        //
-        // shadowColor: Colors.white,
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          //    mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
             Expanded(
               flex:7,
               child: Padding(
@@ -144,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
 
-        Expanded(
+        const Expanded(
           flex:1,
           child:
                 Padding(
--- lib/new_problem.dart
+++ lib/new_problem.dart
@@ -5,6 +5,82 @@ import 'package:google_fonts/google_fonts.dart';
 class NewProblem extends StatelessWidget {
   const NewProblem({Key? key}) : super(key: key);
 
+  Widget _textEntry(String header,String text, String hint, String smallText, int lines) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  header,
+                  style: const TextStyle(
+                      fontSize: 16.5,
+                      color: Colors.black26,
+                      fontWeight: FontWeight.w800),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextField(
+                  style: GoogleFonts.rubik(
+                    textStyle:
+                    const TextStyle(fontSize: 16 , color: Colors.black26), fontWeight: FontWeight.w500),
+                  minLines: lines,
+                  maxLines: lines,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black12, width: 1.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                      ),
+                      border: const OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.black12, width: 1.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.black12, width: 1.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: const TextStyle(
+                          color: Colors.black12,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
+                      hintText: hint,
+                      fillColor: Colors.white),
+                      controller:  TextEditingController(text: text),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  smallText,
+                  style: const TextStyle(
+                      fontSize: 13.5,
+                      color: Colors.black26,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+              ]
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,112 +95,98 @@ class NewProblem extends StatelessWidget {
                 scale: 1.1,
               ),
               onPressed: () {
-             //   Navigator.of(context).push(MaterialPageRoute(builder: (context) => null ));
+                //   Navigator.of(context).push(MaterialPageRoute(builder: (context) => null ));
               },
             ),
           ),
         ],
 
-        title:
-    Center(
-    child:Expanded(
-          child:
-        Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 5, 0, 2),
-                child: Text('Create New Problem', textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 18,fontWeight: FontWeight.w900),
+        title: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                  child: Text(
+                    'Create New Problem',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w900),
+                  ),
                 ),
-              ),
-            ]
+              ]),
         ),
-    ),
-    ),
+
         //
         // shadowColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          //    mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        //    mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25, 30, 25, 2),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
 
-            Expanded(
-              flex:7,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(38, 0, 38, 0),
-                child: Image.asset(
-                  "assets/images/people_image.png",
-                  fit: BoxFit.contain,
-                  alignment: Alignment.center,
-                ),
-              ),
+                _textEntry("Title","", "Enter a title",
+                    "Maximum 30 characters (12 remaining)", 1),
+                _textEntry("Descriptions", "Please, the logo must represent the brand character, "
+                    "positioning, and the client USP and ESP","",
+                    "Maximum 300 characters (132 remaining)", 9),
+              ],
             ),
+          ),
 
-            Expanded(
-              flex:1,
-              child:
-              Padding(
-                padding: EdgeInsets.fromLTRB(70, 0, 70, 0),
-                child: Text(
-                  "Try to assign more tasks to your employees or "
-                      "create a new project from scratch",
-                  style: TextStyle(
-                      fontSize: 14,
-                      height: 1.5,
-                      color: Colors.black38,
-                      fontWeight: FontWeight.w600
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-
-
-            Expanded(
-              flex:2,
-              child:
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(38, 0, 38, 20),
-                          child: SizedBox(
-                            // height: 40,
-                            child: TextButton(
-                                onPressed: () {},
-                                style: ButtonStyle(
-                                    foregroundColor: MaterialStateProperty.all<Color>(
-                                        Colors.white),
-                                    backgroundColor: MaterialStateProperty.all<Color>(
-                                      const Color.fromARGB(255, 95, 100, 222),
-                                    ),
-                                    shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.circular(10.0),
-                                            side: const BorderSide(color: Color.fromARGB(255, 95, 100, 222),)))),
-                                child: const Text("Publish",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w800, fontSize: 15))),
-                          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 0, 38, 20),
+                        child: SizedBox(
+                          // height: 40,
+                          child: TextButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                    const Color.fromARGB(255, 95, 100, 222),
+                                  ),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          side: const BorderSide(
+                                            color: Color.fromARGB(
+                                                255, 95, 100, 222),
+                                          )))),
+                              child: const Text("Publish",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 15))),
                         ),
                       ),
-                    ],
-                  ),
-
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
